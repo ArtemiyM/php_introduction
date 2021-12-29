ed7442c8b2866a93b803b4c7deaa92762d7e2bfa
Morozov Artemiy <nikobelok@yandex.ru>
2021-12-29 03:30:53 +0300

    constructor elem now on the center

--- constructor.php
+++ constructor.php
@@ -1,49 +1,50 @@
 <?php
-    include "template/header.php";
-    include_once "core/db.php";
+include "template/header.php";
+include_once "core/db.php";
 ?>
 
-<?php 
-  if(isset($_POST["albumName"])){
-    //var_dump($_POST);
-    $albumname = htmlspecialchars($_POST["albumName"]);
-    $albumPhotos = $_POST["Photos"];
+<?php
+if (isset($_POST["albumName"])) {
+  //var_dump($_POST);
+  $albumname = htmlspecialchars($_POST["albumName"]);
+  $albumPhotos = $_POST["Photos"];
 
-    $sqlString = "INSERT INTO Albums (name) VALUES ('$albumname')";
+  $sqlString = "INSERT INTO Albums (name) VALUES ('$albumname')";
 
-    try{
-      $albumid = $pdo->exec($sqlString);
-    }catch(PDOException $e){
-      echo $e->getMessage();
-    }
+  try {
+    $albumid = $pdo->exec($sqlString);
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+  }
 
-    foreach ($albumPhotos as $key => $photo){
-      $pdo->exec("INSERT INTO album_photos (photo_id,album_id) VALUES ('$photo','$albumid')");
-    }
+  foreach ($albumPhotos as $key => $photo) {
+    $pdo->exec("INSERT INTO album_photos (photo_id,album_id) VALUES ('$photo','$albumid')");
+  }
 }
 ?>
 
 <form action="" method="POST" style="height: 500px;">
-  <div class="d-flex">
-    <div class="container col-md-6 mx-auto align-middle">
-      <p class="text-center bg-dark text-white">Выберите фотографии и нажмите "сохранить"</p>
+  <div class="row" style="height: 600px;">
+    <div class="container col-sm-5 align-middle align-items-center">
+      <p class="text-center py-2 bg-dark text-white">Выберите фотографии и нажмите "сохранить"</p>
       <div class="mt-2">
         <select class="form-select" name="Photos[]" multiple>
-          <?php foreach ($Photos as $key => $photo){?>
-            <option value="<?php echo $photo['id']?>">
-              photo_number <?php echo $photo['id']?>
+          <?php foreach ($Photos as $key => $photo) { ?>
+            <option value="<?php echo $photo['id'] ?>">
+              photo_number <?php echo $photo['id'] ?>
             </option>
-            <?php }?>
-          </select>
-        </div>
-        <div class="constructorBar">
-          <div class="row text-center mt-2">
-            <input class="form-control col" name="albumName" placeholder="Album name" aria-label="albumName">
-            <button class="btn btn-secondary col">Save</button>
-          </div>
+          <?php } ?>
+        </select>
+      </div>
+      <div class="constructorBar">
+        <div class="row text-center mt-2">
+          <input class="form-control col" name="albumName" placeholder="Album name" aria-label="albumName">
+          <button class="btn btn-secondary col">Save</button>
         </div>
       </div>
+    </div>
   </div>
 </form>
 </body>
+
 </html>
